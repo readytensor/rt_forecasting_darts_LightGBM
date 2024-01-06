88415d2ea2849bdf121ef143ce582a38b5912d1c
moo3030 <mohamedabdelhamid3030@gmail.com>
2024-01-06 15:43:35 +0200

    fixed history length bug

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -164,7 +164,6 @@ class Forecaster:
         self,
         history: pd.DataFrame,
         data_schema: ForecastingSchema,
-        history_length: int = None,
     ) -> Tuple[List, List, List]:
         """
         Puts the data into the expected shape by the forecaster.
@@ -173,7 +172,7 @@ class Forecaster:
         Args:
             history (pd.DataFrame): The provided training data.
             data_schema (ForecastingSchema): The schema of the training data.
-            history_length (int): The number of historical timesteps to be considered.
+
 
         Returns:
             Tuple[List, List, List]: Target, Past covariates and Future covariates.
@@ -206,7 +205,7 @@ class Forecaster:
         self.all_ids = all_ids
         scalers = {}
         for index, s in enumerate(all_series):
-            if history_length:
+            if self.history_length:
                 s = s.iloc[-self.history_length :]
             s.reset_index(inplace=True)
 
@@ -248,7 +247,7 @@ class Forecaster:
         future_scalers = {}
         if future_covariates_names:
             for id, train_series in zip(all_ids, all_series):
-                if history_length:
+                if self.history_length:
                     train_series = train_series.iloc[-self.history_length :]
 
                 future_covariates = train_series[future_covariates_names]
@@ -354,7 +353,6 @@ class Forecaster:
         self,
         history: pd.DataFrame,
         data_schema: ForecastingSchema,
-        history_length: int = None,
     ) -> None:
         """Fit the Forecaster to the training data.
         A separate LinearRegression model is fit to each series that is contained
@@ -363,12 +361,11 @@ class Forecaster:
         Args:
             history (pandas.DataFrame): The features of the training data.
             data_schema (ForecastingSchema): The schema of the training data.
-            history_length (int): The length of the series used for training.
+
         """
         np.random.seed(self.random_state)
         targets, past_covariates, future_covariates = self._prepare_data(
             history=history,
-            history_length=history_length,
             data_schema=data_schema,
         )
 
